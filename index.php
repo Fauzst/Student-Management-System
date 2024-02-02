5178e953662297cd286220219b6485cb33bc1d45
Joshua Elijah Pura <business.joshua.pura@gmail.com>
2024-02-02 23:14:43 +0800

    Login box finished designing

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+    <?php include 'styles.css'; ?>
+</style>
+<body>
+    <div class="container--login">
+        <div class="announcement--box"></div>
+        <div class="login--box">
+            <img class="login--header" src="assets/images/pup-school.jpg"></img>
+            <h1>Please login</h1>
+            <form class="inputs">
+                <input class="login--inputs" type="name" name="username" placeholder="Username"></input>
+                <input class="login--inputs" type="password" name="password" placeholder="Password"></input>
+                <input class="login--inputs blue" type="submit" name="submit" value="Login"></input>
+            </form>
+            <a href="#" >Forgot Password?</a>
+        </div>
+    </div>
+</body>
+</html>
