9689c9748563f81fbb4412dd1d909da6b5a7cb5c
Joshua Elijah Pura <business.joshua.pura@gmail.com>
2024-02-04 22:40:03 +0800

    with bootstrap

---
+++ my-info.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/images/css/bootstrap.min.css">
+    
+    <script src="assets/images/js/bootstrap.bundle.min.js"></script>
+    <style>
+      [class*="col"] {
+        padding: 1rem;
+        background-color: #33b5e5;
+        border: 2px solid #fff;
+        color: #fff;
+    
+      }
+    </style>
+</head>
+<body>
+    
+</body>
+</html>
