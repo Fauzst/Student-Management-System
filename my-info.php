cf9fcac85168f85d1138cfd3dd7a2d94bb65e1f7
Joshua Elijah Pura <business.joshua.pura@gmail.com>
2024-02-08 00:21:03 +0800

    Dashboard 40% design complete

--- my-info.php
+++ my-info.php
@@ -4,20 +4,115 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="assets/images/css/bootstrap.min.css">
+    <link rel="stylesheet" href="my-info.css">
     
-    <script src="assets/images/js/bootstrap.bundle.min.js"></script>
-    <style>
-      [class*="col"] {
-        padding: 1rem;
-        background-color: #33b5e5;
-        border: 2px solid #fff;
-        color: #fff;
     
-      }
-    </style>
 </head>
 <body>
-    
+    <div class="container--dashboard">
+      <div class="logo">Polytechnic University of the Philippines</div>
+      <div class="nav">
+        <ul>
+          <li>My Info</li>
+          <li>Course Enlistment/Enrollment</li>
+          <li>Grade Viewing</li>
+          <li>Faculty Evaluation</li>
+          <li>Signout</li>
+        </ul>
+      </div>
+      <div class="semester">
+        <button>2023-2024/2nd Semester</button>
+      </div>
+      <div class="main-content">
+        <h1>Account Details</h1>
+        <form action="#" class="information">
+          <div class="info--card">
+            <div class="element--card">Student ID</div>
+            <input type="text" class="input--card" placeholder="1012301321" disabled/>
+          </div>
+
+          <div class="info--card">
+            <div class="element--card">Email Address</div>
+            <input type="text" class="input--card" placeholder="juandelacruz@gmail.com" disabled/>
+          </div>
+
+          <div class="info--card">
+            <div class="element--card">Contact Number</div>
+            <input type="text" class="input--card" placeholder="0912 345 6789" disabled/>
+          </div>
+
+          <div class="info--card">
+            <div class="element--card">Program of Study</div>
+            <input type="text" class="input--card" placeholder="Bachelor of Science in Computer Engineering" disabled/>
+          </div>
+
+          <div class="info--card">
+            <div class="element--card">Learner's Reference Number</div>
+            <input type="text" class="input--card" placeholder="2022101010" disabled/>
+          </div>
+        </form>
+
+        <hr>
+
+        <h1>Personal Information</h1>
+        <form action="#" class="information">
+          <div class="info--card">
+            <div class="element--card">Student ID</div>
+            <input type="text" class="input--card" placeholder="1012301321" disabled/>
+          </div>
+
+          <div class="info--card">
+            <div class="element--card">Email Address</div>
+            <input type="text" class="input--card" placeholder="juandelacruz@gmail.com" disabled/>
+          </div>
+
+          <div class="info--card">
+            <div class="element--card">Contact Number</div>
+            <input type="text" class="input--card" placeholder="0912 345 6789" disabled/>
+          </div>
+
+          <div class="info--card">
+            <div class="element--card">Program of Study</div>
+            <input type="text" class="input--card" placeholder="Bachelor of Science in Computer Engineering" disabled/>
+          </div>
+
+          <div class="info--card">
+            <div class="element--card">Learner's Reference Number</div>
+            <input type="text" class="input--card" placeholder="2022101010" disabled/>
+          </div>
+        </form>
+
+        <hr>
+
+        <h1>Family Information</h1>
+        <form action="#" class="information">
+          <div class="info--card">
+            <div class="element--card">Student ID</div>
+            <input type="text" class="input--card" placeholder="1012301321" disabled/>
+          </div>
+
+          <div class="info--card">
+            <div class="element--card">Email Address</div>
+            <input type="text" class="input--card" placeholder="juandelacruz@gmail.com" disabled/>
+          </div>
+
+          <div class="info--card">
+            <div class="element--card">Contact Number</div>
+            <input type="text" class="input--card" placeholder="0912 345 6789" disabled/>
+          </div>
+
+          <div class="info--card">
+            <div class="element--card">Program of Study</div>
+            <input type="text" class="input--card" placeholder="Bachelor of Science in Computer Engineering" disabled/>
+          </div>
+
+          <div class="info--card">
+            <div class="element--card">Learner's Reference Number</div>
+            <input type="text" class="input--card" placeholder="2022101010" disabled/>
+          </div>
+        </form>
+
+      </div>
+    </div>
 </body>
 </html>
