ab28390ec244dfa8fc34552179c9af2b5f0c32fc
Joshua Elijah Pura <business.joshua.pura@gmail.com>
2024-02-08 10:48:40 +0800

    Info Dashboard 60%, Navigation 90%

--- my-info.php
+++ my-info.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="my-info.css">
+    <link rel="stylesheet" href="my-info-style.css">
     
     
 </head>
 <body>
     <div class="container--dashboard">
-      <div class="logo">Polytechnic University of the Philippines</div>
+      <div class="logo"><p>Polytechnic University of the Philippines</p></div>
       <div class="nav">
         <ul>
           <li>My Info</li>
@@ -57,29 +57,85 @@
         <h1>Personal Information</h1>
         <form action="#" class="information">
           <div class="info--card">
-            <div class="element--card">Student ID</div>
-            <input type="text" class="input--card" placeholder="1012301321" disabled/>
+            <div class="element--card">Last Name</div>
+            <input type="text" class="input--card" placeholder="Sins" disabled/>
           </div>
 
           <div class="info--card">
-            <div class="element--card">Email Address</div>
-            <input type="text" class="input--card" placeholder="juandelacruz@gmail.com" disabled/>
+            <div class="element--card">First Name</div>
+            <input type="text" class="input--card" placeholder="Jhonny" disabled/>
           </div>
 
           <div class="info--card">
-            <div class="element--card">Contact Number</div>
-            <input type="text" class="input--card" placeholder="0912 345 6789" disabled/>
+            <div class="element--card">Suffix</div>
+            <input type="text" class="input--card" placeholder="Jr." disabled/>
           </div>
 
           <div class="info--card">
-            <div class="element--card">Program of Study</div>
-            <input type="text" class="input--card" placeholder="Bachelor of Science in Computer Engineering" disabled/>
+            <div class="element--card">Birthdate</div>
+            <input type="text" class="input--card" placeholder="2003-20-12" disabled/>
           </div>
 
           <div class="info--card">
-            <div class="element--card">Learner's Reference Number</div>
-            <input type="text" class="input--card" placeholder="2022101010" disabled/>
+            <div class="element--card">Birthplace</div>
+            <input type="text" class="input--card" placeholder="Quezon City" disabled/>
           </div>
+
+          <div class="info--card">
+            <div class="element--card">Sex</div>
+            <input type="text" class="input--card" placeholder="Male" disabled/>
+          </div>
+
+          <div class="info--card">
+            <div class="element--card">Nationality</div>
+            <input type="text" class="input--card" placeholder="Filipino" disabled/>
+          </div>
+
+          <div class="info--card">
+            <div class="element--card">Civil Status</div>
+            <input type="text" class="input--card" placeholder="Single" disabled/>
+          </div>
+
+          <div class="info--card">
+            <div class="element--card">Religion</div>
+            <input type="text" class="input--card" placeholder="Catholic" disabled/>
+          </div>
+        </form>
+
+        <hr>
+
+        <h1>Address</h1>
+        <form action="#" class="information">
+          <div class="info--card">
+            <div class="element--card">Block Number</div>
+            <input type="text" class="input--card" placeholder="1" disabled/>
+          </div>
+
+          <div class="info--card">
+            <div class="element--card">Street</div>
+            <input type="text" class="input--card" placeholder="Sabungan street" disabled/>
+          </div>
+
+          <div class="info--card">
+            <div class="element--card">Subdivision</div>
+            <input type="text" class="input--card" placeholder="N/A" disabled/>
+          </div>
+
+          <div class="info--card">
+            <div class="element--card">City</div>
+            <input type="text" class="input--card" placeholder="Quezon City" disabled/>
+          </div>
+
+          <div class="info--card">
+            <div class="element--card">District</div>
+            <input type="text" class="input--card" placeholder="District 1" disabled/>
+          </div>
+
+          <div class="info--card">
+            <div class="element--card">Zipcode</div>
+            <input type="text" class="input--card" placeholder="1090" disabled/>
+          </div>
+
         </form>
 
         <hr>
