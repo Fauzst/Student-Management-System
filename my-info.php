427dfe8075fb78f542e32beea41020fe04b3c988
Joshua Elijah Pura <business.joshua.pura@gmail.com>
2024-02-08 23:27:17 +0800

    Dashboard 60% complete

--- my-info.php
+++ my-info.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="my-info-style.css">
+    <link rel="stylesheet" href="my-info.css">
     
     
 </head>
@@ -13,10 +13,10 @@
       <div class="logo"><p>Polytechnic University of the Philippines</p></div>
       <div class="nav">
         <ul>
-          <li>My Info</li>
-          <li>Course Enlistment/Enrollment</li>
-          <li>Grade Viewing</li>
-          <li>Faculty Evaluation</li>
+          <li class="active"><img src="assets/images/user-regular.svg" width="10px"></img>  My Info</li>
+          <li><img src="assets/images/list-solid.svg" width="12px">  Course Enlistment/Enrollment</li>
+          <li><img src="assets/images/book-solid.svg" width="12px"> Grade Viewing</li>
+          <li><img src="assets/images/comments-solid.svg" width="12px"> Faculty Evaluation</li>
           <li>Signout</li>
         </ul>
       </div>
@@ -24,6 +24,7 @@
         <button>2023-2024/2nd Semester</button>
       </div>
       <div class="main-content">
+        
         <h1>Account Details</h1>
         <form action="#" class="information">
           <div class="info--card">
@@ -53,9 +54,10 @@
         </form>
 
         <hr>
-
-        <h1>Personal Information</h1>
-        <form action="#" class="information">
+        <div class="header-edit">
+        <h1>Personal Information</h1> <button id="edit-submit" onclick="editInformation()">Edit</button>
+      </div>
+        <form action="#" class="information" id="personal-info">
           <div class="info--card">
             <div class="element--card">Last Name</div>
             <input type="text" class="input--card" placeholder="Sins" disabled/>
@@ -170,5 +172,35 @@
 
       </div>
     </div>
+
+    <script>
+      function editInformation(){
+        var form = document.getElementById("personal-info");
+        var inputs = form.getElementsByTagName("input");
+        var button = document.querySelector(".header-edit");
+        for(let i = 0; i < inputs.length; i ++){
+          inputs[i].removeAttribute("disabled")
+        }
+
+        var submitInfo = document.createElement("button")
+        submitInfo.innerHTML = "Submit";
+        submitInfo.setAttribute("type", "submit");
+        submitInfo.setAttribute("id", "submit-info")
+        submitInfo.setAttribute("onclick", "submitInfo()");
+        button.appendChild(submitInfo);
+    
+      }
+
+      function submitInfo(){
+        var element = document.getElementById("submit-info");
+
+        if (element){
+          element.remove();
+        }
+        
+
+      }
+
+    </script>
 </body>
 </html>
